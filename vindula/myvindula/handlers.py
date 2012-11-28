3d15bec277be883058464b41a61edf7b43698a26
Denis Raison <denisraison@liberiun.com>
2012-11-28 10:48:51 -0200

    Correcao para cair na tela de primeiro login quando estiver desabilitado

--- vindula/myvindula/handlers.py
+++ vindula/myvindula/handlers.py
@@ -28,44 +28,43 @@ def userupdate(event):
         user_id = to_utf8(user_login.getUserName())
     except:
         user_id = user_login.getUserName()
-        
-    if not ModelsFuncDetails().get_FuncDetails(user_id) or\
-       user_id != 'admin':
-       #not ModelsUserOpenFire().get_UserOpenFire_by_username(user_id) and\
-       
-        
-        if not ModelsFuncDetails().get_FuncDetails(user_id):
-            D = {}
-            D['username'] = user_id
-            if user_login.getProperty('fullname'):
-                user = user_login.getProperty('fullname')
-            else:user = user_id
+
+    if enable:
+        if not ModelsFuncDetails().get_FuncDetails(user_id) or\
+           user_id != 'admin':
+           #not ModelsUserOpenFire().get_UserOpenFire_by_username(user_id) and\
+            if not ModelsFuncDetails().get_FuncDetails(user_id):
+                D = {}
+                D['username'] = user_id
+                if user_login.getProperty('fullname'):
+                    user = user_login.getProperty('fullname')
+                else:user = user_id
+                
+                try:D['name'] = to_utf8(user)
+                except:D['name'] = user
+                
+                try:D['email'] = to_utf8(user_login.getProperty('email'))
+                except:D['email'] = user_login.getProperty('email')
+                
+                ModelsFuncDetails().set_FuncDetails(**D)
+                logger.info("Usuario criado no myvindula")
             
-            try:D['name'] = to_utf8(user)
-            except:D['name'] = user
-            
-            try:D['email'] = to_utf8(user_login.getProperty('email'))
-            except:D['email'] = user_login.getProperty('email')
-            
-            ModelsFuncDetails().set_FuncDetails(**D)
-            logger.info("Usuario criado no myvindula")
-        
-        if not ModelsUserOpenFire().get_UserOpenFire_by_username(user_id):
-            CreateUserXMPP(user_id)
-            
-        if not request.other.get('came_from') or request.other.get('came_from') == getSite().portal_url()+'/':
-            request.other["came_from"]=registro_url
-        request.response.redirect(registro_url, lock=True)
-        
-    else:
-        user_data = ModelsFuncDetails().get_FuncDetails(user_id)
-        
-        if ((not user_data.name) or (not user_data.date_birth) or\
-            (not user_data.phone_number) or (not user_data.email)) and enable:
-            
-            logger.info("Dados Incompletos no myvindula")
-            
+            if not ModelsUserOpenFire().get_UserOpenFire_by_username(user_id):
+                CreateUserXMPP(user_id)
+                
             if not request.other.get('came_from') or request.other.get('came_from') == getSite().portal_url()+'/':
                 request.other["came_from"]=registro_url
             request.response.redirect(registro_url, lock=True)
+            
+        else:
+            user_data = ModelsFuncDetails().get_FuncDetails(user_id)
+            
+            if ((not user_data.name) or (not user_data.date_birth) or\
+                (not user_data.phone_number) or (not user_data.email)) and enable:
+                
+                logger.info("Dados Incompletos no myvindula")
+                
+                if not request.other.get('came_from') or request.other.get('came_from') == getSite().portal_url()+'/':
+                    request.other["came_from"]=registro_url
+                request.response.redirect(registro_url, lock=True)
             
