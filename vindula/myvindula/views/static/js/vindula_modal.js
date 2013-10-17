fdd2a961fa26ff9db3098e61fa2f5fee4fbc1d10
Denis Raison <denisraison@liberiun.com>
2013-10-17 14:54:04 -0300

    Correcao no modal de pessoas e unidades

--- vindula/myvindula/views/static/js/vindula_modal.js
+++ vindula/myvindula/views/static/js/vindula_modal.js
@@ -6,59 +6,116 @@ function  createElement(name) {
 
 function defineLinkModal() {
     
-    $j('a.profile-link, a.structure-link').hover(function (ev) {
-        clearTimeout($j(this).data('timeout_out'));
-        var $el = $j(this),
-            JQ_height = $el.height() || 0,
-            JS_height = this.height || 0;
-        
-        
-        var height = Math.max(JQ_height, JS_height);
-        
-        var t_in = setTimeout(function() {
-            if($el.find('.vindula-modal').length){
-                $el.find('.vindula-modal').show();
-            }else{
-                var content_value = $el.attr('data-value'),
-                    base_url = $j('base').attr('href'),
-                    type_of_modal = $el.attr('data-type');
-                
-                if (type_of_modal == 'UserObject'){
-                    modal_url = base_url+'/modal-profile';
+    $j('a.profile-link, a.structure-link').live({
+        mouseenter: function (ev) {
+            clearTimeout($j(this).data('timeout_out'));
+            var $el = $j(this),
+                JQ_height = $el.height() || 0,
+                JS_height = this.height || 0;
+            
+            
+            var height = Math.max(JQ_height, JS_height);
+            
+            var t_in = setTimeout(function() {
+                if($el.find('.vindula-modal').length){
+                    $el.find('.vindula-modal').show();
                 }else{
-                    modal_url = base_url+'/modal-structure';
-                }
-    
-                $j.ajax({
-                    type: "POST",
-                    url: modal_url,
-                    data: {content_value: content_value},
-                    success: function(data){
-                        $dom = $j(data);
-                        $dom.css('top', height+10);
-                        $dom.find('.social-info').vindula(null, {user_token: window.token});
-                        $el.append($dom);
+                    var content_value = $el.attr('data-value'),
+                        base_url = $j('base').attr('href'),
+                        type_of_modal = $el.attr('data-type');
+                    
+                    if (type_of_modal == 'UserObject'){
+                        modal_url = base_url+'/modal-profile';
+                    }else{
+                        modal_url = base_url+'/modal-structure';
                     }
-//                    error: function(error){
-//                        console.error(error);
-//                    }
-                })
-            }
-        }, 1000);
         
-        $j(this).data('timeout_in', t_in);
-    },function (ev) {
-        clearTimeout($j(this).data('timeout_in'));
+                    $j.ajax({
+                        type: "POST",
+                        url: modal_url,
+                        data: {content_value: content_value},
+                        success: function(data){
+                            $dom = $j(data);
+                            $dom.css('top', height+10);
+                            $dom.find('.social-info').vindula(null, {user_token: window.token});
+                            $el.append($dom);
+                        }
+    //                    error: function(error){
+    //                        console.error(error);
+    //                    }
+                    })
+                }
+            }, 1000);
         
-        var $el = $j(this);
-        var t_out = setTimeout(function() {
-            if ($el.find('.vindula-modal').length) {
-                $el.find('.vindula-modal').hide();
-            }
-        }, 500);
+            $j(this).data('timeout_in', t_in);
+        },
+        mouseleave: function (ev) {
+            clearTimeout($j(this).data('timeout_in'));
         
-        $j(this).data('timeout_out', t_out);
+            var $el = $j(this);
+            var t_out = setTimeout(function() {
+                if ($el.find('.vindula-modal').length) {
+                    $el.find('.vindula-modal').hide();
+                }
+            }, 500);
+            
+            $j(this).data('timeout_out', t_out);
+        }
     });
+    
+//    $j('a.profile-link, a.structure-link').hover(function (ev) {
+//        clearTimeout($j(this).data('timeout_out'));
+//        var $el = $j(this),
+//            JQ_height = $el.height() || 0,
+//            JS_height = this.height || 0;
+//        
+//        
+//        var height = Math.max(JQ_height, JS_height);
+//        
+//        var t_in = setTimeout(function() {
+//            if($el.find('.vindula-modal').length){
+//                $el.find('.vindula-modal').show();
+//            }else{
+//                var content_value = $el.attr('data-value'),
+//                    base_url = $j('base').attr('href'),
+//                    type_of_modal = $el.attr('data-type');
+//                
+//                if (type_of_modal == 'UserObject'){
+//                    modal_url = base_url+'/modal-profile';
+//                }else{
+//                    modal_url = base_url+'/modal-structure';
+//                }
+//    
+//                $j.ajax({
+//                    type: "POST",
+//                    url: modal_url,
+//                    data: {content_value: content_value},
+//                    success: function(data){
+//                        $dom = $j(data);
+//                        $dom.css('top', height+10);
+//                        $dom.find('.social-info').vindula(null, {user_token: window.token});
+//                        $el.append($dom);
+//                    }
+////                    error: function(error){
+////                        console.error(error);
+////                    }
+//                })
+//            }
+//        }, 1000);
+//        
+//        $j(this).data('timeout_in', t_in);
+//    },function (ev) {
+//        clearTimeout($j(this).data('timeout_in'));
+//        
+//        var $el = $j(this);
+//        var t_out = setTimeout(function() {
+//            if ($el.find('.vindula-modal').length) {
+//                $el.find('.vindula-modal').hide();
+//            }
+//        }, 500);
+//        
+//        $j(this).data('timeout_out', t_out);
+//    });
     
 }
 
