b5200e61cecdf49b9cec5ccc9e021908d2cd633a
Cesar Augusto <cesaraugusto@liberiun.com>
2014-05-05 13:11:54 -0300

    bugfix merge

--- vindula/myvindula/models/base.py
+++ vindula/myvindula/models/base.py
@@ -7,13 +7,11 @@ from storm.expr import Desc, Select
 from storm.zope.interfaces import IZStorm
 from zope.component import getUtility
 from datetime import date , datetime, timedelta
-<<<<<<< HEAD
+
 from hashlib import md5
 from random import choice
-=======
 from Products.CMFCore.utils import getToolByName
 from zope.app.component.hooks import getSite
->>>>>>> 64b82b56b397644671dc1f36cce1a766b5c789a1
 
 #import sys
 #from storm.tracer import debug #debug(True, stream=sys.stdout)
@@ -60,20 +58,6 @@ class BaseStoreMyvindula(BaseStore):
     
     @property
     def date_creation(self):
-<<<<<<< HEAD
-        return self.date_created
-
-
-    @property
-    def __hash_vindulapp__(self):
-        if not self.id:
-            #TODO: Gerar um id aleatorio temporariamente
-            id = datetime.now().strftime('%Y-%m-%d|%H:%M:%S')+str(choice(range(100)))
-            hash = md5(self.__class__.__name__ + str(id)).hexdigest()
-        else:
-            hash = md5(self.__class__.__name__ + str(self.id)).hexdigest()
-        return unicode(hash, 'utf-8')
-=======
         time_zone = BaseStoreMyvindula.getVindulaTimeZone()
         date = self.date_created - timedelta(hours=time_zone)
         return date
@@ -91,4 +75,14 @@ class BaseStoreMyvindula(BaseStore):
                 time_zone = record_time_zone.value
                 
         return time_zone
->>>>>>> 64b82b56b397644671dc1f36cce1a766b5c789a1
+
+
+    @property
+    def __hash_vindulapp__(self):
+        if not self.id:
+            #TODO: Gerar um id aleatorio temporariamente
+            id = datetime.now().strftime('%Y-%m-%d|%H:%M:%S')+str(choice(range(100)))
+            hash = md5(self.__class__.__name__ + str(id)).hexdigest()
+        else:
+            hash = md5(self.__class__.__name__ + str(self.id)).hexdigest()
+        return unicode(hash, 'utf-8')
