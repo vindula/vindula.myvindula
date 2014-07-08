d6fc682c52240062076ed9f2e142ec5d6a5358f7
Cesar Augusto <cesaraugusto@liberiun.com>
2014-07-08 09:50:12 -0300

    Criação do webserve de exportação das fotos dos usuarios

---
+++ vindula/myvindula/browser/export_photouser.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from five import grok
+from zope.interface import Interface
+from vindula.myvindula.user import BaseFunc
+from vindula.myvindula.models.photo_user import ModelsPhotoUser
+
+import json
+
+# Metodo que criar o usuario no acl_user do plone 
+class VindulaWebServeExportPhotoUser(grok.View,BaseFunc):
+    grok.context(Interface)
+    grok.name('vindula-export-photouser')
+    grok.require('zope2.View')
+
+    retorno = {}
+
+    def render(self):
+        self.request.response.setHeader("Content-type","application/json")
+        self.request.response.setHeader("charset", "UTF-8")
+        return json.dumps(self.retorno,ensure_ascii=False)
+
+    def update(self):
+        dados = {}
+        username = self.Convert_utf8(self.request.form.get('username',''))
+        field = u'photograph'
+
+        campo_image = ModelsPhotoUser().get_ModelsPhotoUser_byUsername(username,field)
+
+        if campo_image:
+            self.retorno['photograph'] = campo_image.photograph
+            self.retorno['thumb'] = campo_image.thumb
+
+        
