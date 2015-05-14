bb457a4435ed807f527d10f1731c6a931f9d37f7
Denis Raison <denisraison@liberiun.com>
2015-05-14 16:37:58 -0300

    Ajustes no login

--- vindula/myvindula/handlers.py
+++ vindula/myvindula/handlers.py
@@ -14,7 +14,7 @@ def userupdate(event):
     """ Handler for User Login in Site """
     tools = UtilMyvindula()
     user_login = tools.membership.getAuthenticatedMember()
-    alert_first_access = tools.site.restrictedTraverse('@@myvindula-conf-userpanel').check_alert_first_access()
+    # alert_first_access = tools.site.restrictedTraverse('@@myvindula-conf-userpanel').check_alert_first_access()
 
     # enable_chat = tools.site.restrictedTraverse('vindula-chat-config').enableConf()
     user_schema = ModelsDadosFuncdetails()
@@ -24,7 +24,7 @@ def userupdate(event):
     user_instance = ModelsDadosFuncdetails().get_DadosFuncdetails_byInstance(user_id)
 
     #Se nao tiver perfil, cria o perfil
-    if alert_first_access and not user_instance:
+    if not user_instance:
 
         dados = {u'username':user_id,
                  u'email':user_login.getProperty('email'),
@@ -34,10 +34,9 @@ def userupdate(event):
             dados['name'] = user_login.getProperty('fullname')
 
         user_schema.createUserProfile(dados)
-
-
-    else:
-        user_data =  tools.get_prefs_user(user_id)
-        if ((not user_data.get('name')) or (not user_data.get('date_birth')) or\
-            (not user_data.get('phone_number')) or (not user_data.get('email'))) and alert_first_access:
-            tools.setLogger('info',"Dados Incompletos no myvindula")
+        
+    # else:
+    #     user_data =  tools.get_prefs_user(user_id)
+    #     if ((not user_data.get('name')) or (not user_data.get('date_birth')) or\
+    #         (not user_data.get('phone_number')) or (not user_data.get('email'))):
+    #         tools.setLogger('info',"Dados Incompletos no myvindula")
